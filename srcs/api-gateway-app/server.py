9267698297f4614ae06e43735e26fba1ad43f026
EAndreyc <andreychenko@inbox.ru>
2025-11-10 16:59:24 +0300

    add Welcome to localhost page:)

--- srcs/api-gateway-app/server.py
+++ srcs/api-gateway-app/server.py
@@ -1,7 +1,17 @@
 import os
+from flask import jsonify
 from app import create_app
 
 app = create_app()
+
+# --- Добавляем приветственный маршрут ---
+@app.route("/")
+def home():
+    return jsonify({
+        "message": "Welcome to Play with Containers API Gateway 🚀",
+        "status": "running",
+        "endpoints": ["/health", "/inventory", "/billing"]
+    })
 
 if __name__ == '__main__':
     port = int(os.getenv('APP_PORT', 3000))
